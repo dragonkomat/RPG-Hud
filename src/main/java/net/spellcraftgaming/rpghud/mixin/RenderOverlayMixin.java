ae46dcfb860e3b328d8fe79c8e3aebf637d76d99
dragonkomat <github@komat.info>
2021-09-12 14:36:31 +0900

    Change render mixin. Remove deprecated method call.

--- src/main/java/net/spellcraftgaming/rpghud/mixin/RenderOverlayMixin.java
+++ src/main/java/net/spellcraftgaming/rpghud/mixin/RenderOverlayMixin.java
@@ -3,28 +3,32 @@ package net.spellcraftgaming.rpghud.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+//import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.gui.hud.InGameHud;
+//import net.minecraft.client.util.math.MatrixStack;
 import net.spellcraftgaming.rpghud.gui.hud.element.HudElementType;
 import net.spellcraftgaming.rpghud.main.RenderOverlay;
 
 @Mixin(InGameHud.class)
 public class RenderOverlayMixin extends DrawableHelper {
-    private int ticks;
 
-    @Inject(at = @At("HEAD"), method = "tick")
-    private void tick(CallbackInfo info) {
-        ++this.ticks;
-        ;
+    // TODO: Following redirection causes exception.
+    //   Caused by: org.spongepowered.asm.mixin.injection.throwables.InjectionError: Critical injection failure:
+    //   Redirector onDrawTexture(Lnet/minecraft/class_4587;IIIIII)V in rpg-hud.mixins.json:RenderOverlayMixin failed injection check, (0/1) succeeded. Scanned 1 target(s). Using refmap [1.17.1][Fabric]RPG-HUD-refmap.json
+
+    //@Redirect(method = "renderStatusBars", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawableHelper;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V"))
+    //private void onDrawTexture(DrawableHelper obj, MatrixStack matrices, int x, int y, int u, int v, int width, int height) {
+    //    obj.drawTexture(matrices, x, y, u, v, width, height);
+    //}
+
+    @Inject(at = @At("HEAD"), method = "renderHealthBar", cancellable = true)
+    private void renderHealthBar(CallbackInfo info) {
+        if(!RenderOverlay.shouldRenderVanilla(HudElementType.HEALTH))
+            info.cancel();
     }
-
-// TODO:
-//    @Inject(at = @At("HEAD"), method = "renderStatusBars", cancellable = true)
-//    private void renderStatusBars(CallbackInfo info) {
-//        info.cancel();
-//    }
 
     @Inject(at = @At("HEAD"), method = "renderHotbar", cancellable = true)
     private void renderHotbar(CallbackInfo info) {
